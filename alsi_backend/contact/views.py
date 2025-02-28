96bfdaad467274c7c0f1ad56cc5a47f476937fe8
silvia-marketbytes <silvia@marketbyteswebworks.com>
2025-02-28 10:45:43 +0530

    .

--- alsi_backend/contact/views.py
+++ alsi_backend/contact/views.py
@@ -1,3 +1,5 @@
+
+
 from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -120,11 +122,9 @@ class ContactFormViewSet(viewsets.ModelViewSet):
                 f"Submitted URL: {contact.submitted_url}\n"
             )
 
-            recipients = [
-                'alsiglobalofficial@gmail.com',
-               
-            ]
-            bcc_list = ['gowtham@marketbytes.in', 'ananthu@marketbytes.in','marketbytesofficial@gmail.com']
+            recipients = settings.CONTACT_RECIPIENTS
+            bcc_list = settings.CONTACT_BCC_LIST
+
             email = EmailMessage(
                 subject=subject,
                 body=message,
